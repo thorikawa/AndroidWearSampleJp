4f3d5c5573de016863d7348cbea8e217cf52cb26
Poly Takahiro Horikawa <horikawa.takahiro@gmail.com>
2014-07-24 00:43:22 -0700

    Clean up code

--- mobile/src/main/java/com/polysfactory/androidwearsamplejp/DataActivity.java
+++ mobile/src/main/java/com/polysfactory/androidwearsamplejp/DataActivity.java
@@ -5,8 +5,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,7 +13,7 @@ import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataItem;
-import com.google.android.gms.wearable.DataItemBuffer;
+import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.Node;
@@ -44,46 +42,21 @@ public class DataActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                    @Override
-                    public void onConnected(Bundle bundle) {
-                    }
-
-                    @Override
-                    public void onConnectionSuspended(int i) {
-
-                    }
-                }).build();
-        mGoogleApiClient.connect();
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        ((Button) findViewById(R.id.button_send_count)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 PutDataMapRequest dataMap = PutDataMapRequest.create(COUNT_PATH);
-                Log.d(TAG, "put url:" + dataMap.getUri());
-                dataMap.getDataMap().putInt(COUNT_KEY, count++);
+                dataMap.getDataMap().putInt(COUNT_KEY, ++count);
                 PutDataRequest request = dataMap.asPutDataRequest();
                 PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi
                         .putDataItem(mGoogleApiClient, request);
-                Log.d(TAG, "data start sending" + request.getUri());
                 pendingResult.setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
                     @Override
                     public void onResult(DataApi.DataItemResult dataItemResult) {
-                        Log.d(TAG, "changed:" + dataItemResult.getDataItem().getUri());
-                        Log.d(TAG, "count:" + count);
+                        Log.d(TAG, "count updated:" + count);
                     }
                 });
 
-            }
-        });
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                runAsyncTask();
             }
         });
 
@@ -99,26 +72,28 @@ public class DataActivity extends Activity {
                 }.execute();
             }
         });
-    }
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        Log.d(TAG, "Google Api Client connected");
+                        new AsyncTask<Void, Void, Void>() {
+                            @Override
+                            protected Void doInBackground(Void... voids) {
+                                restoreCurrentCount();
+                                return null;
+                            }
+                        }.execute();
+                    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                    }
+                }).build();
+        mGoogleApiClient.connect();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private String getLocalNodeId() {
@@ -126,48 +101,20 @@ public class DataActivity extends Activity {
         return nodeResult.getNode().getId();
     }
 
-    private String getRemoteNodeId() {
-        HashSet<String> results = new HashSet<String>();
-        NodeApi.GetConnectedNodesResult nodesResult =
-                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
-        List<Node> nodes = nodesResult.getNodes();
-        if (nodes.size() > 0) {
-            return nodes.get(0).getId();
-        }
-        return null;
-    }
-
-    private void runAsyncTask() {
-        new AsyncTask<Void, Void, Void>() {
+    private void restoreCurrentCount() {
+        String localNodeId = getLocalNodeId();
+        Uri uri = new Uri.Builder().scheme(PutDataRequest.WEAR_URI_SCHEME).authority(localNodeId).path(COUNT_PATH).build();
+        Wearable.DataApi.getDataItem(mGoogleApiClient, uri).setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
             @Override
-            protected Void doInBackground(Void... voids) {
-                DataItemBuffer dataItemBuffer = Wearable.DataApi.getDataItems(mGoogleApiClient).await();
-                for (DataItem dataItem : dataItemBuffer) {
-                    Log.d(TAG, "url=[" + dataItem.getUri().toString() + "]");
-                    Log.d(TAG, "val=[" + DataMapItem.fromDataItem(dataItem).getDataMap().get(COUNT_KEY) + "]");
+            public void onResult(DataApi.DataItemResult dataItemResult) {
+                DataItem dataItem = dataItemResult.getDataItem();
+                if (dataItem != null) {
+                    DataMap dataMap = DataMapItem.fromDataItem(dataItemResult.getDataItem()).getDataMap();
+                    count = dataMap.getInt(COUNT_KEY);
+                    Log.d(TAG, "restored count:" + count);
                 }
-
-                Uri uri = new Uri.Builder().scheme(PutDataRequest.WEAR_URI_SCHEME).authority("abcd77b0-5828-4f46-b94e-3a3df9e97f98").path(COUNT_PATH).build();
-                Log.d(TAG, "access to:[" + uri.toString() + "]");
-                Wearable.DataApi.getDataItem(mGoogleApiClient, uri).setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
-                    @Override
-                    public void onResult(DataApi.DataItemResult dataItemResult) {
-                        DataItem dataItem = dataItemResult.getDataItem();
-                        Log.d(TAG, "local dataItemResult:" + dataItem);
-                        if (dataItem != null) {
-                            count = DataMapItem.fromDataItem(dataItemResult.getDataItem()).getDataMap().getInt(COUNT_KEY);
-                            Log.d(TAG, "stored count:" + count);
-                        }
-                    }
-                });
-
-
-                Log.d(TAG, "local nodes:" + getLocalNodeId());
-                String nodes = getRemoteNodeId();
-                Log.d(TAG, "remote nodes:" + nodes.toString());
-                return null;
             }
-        }.execute();
+        });
     }
 
     private void sendMessageToStartActivity() {
@@ -179,6 +126,17 @@ public class DataActivity extends Activity {
                 Log.e(TAG, "ERROR: failed to send Message: " + result.getStatus());
             }
         }
+    }
+
+    private String getRemoteNodeId() {
+        HashSet<String> results = new HashSet<String>();
+        NodeApi.GetConnectedNodesResult nodesResult =
+                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+        List<Node> nodes = nodesResult.getNodes();
+        if (nodes.size() > 0) {
+            return nodes.get(0).getId();
+        }
+        return null;
     }
 
     private Collection<String> getNodes() {
