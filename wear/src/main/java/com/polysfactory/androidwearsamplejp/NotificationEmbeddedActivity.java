3aa3c56828e8785907094bc0f7cc529d51956565
Poly Takahiro Horikawa <horikawa.takahiro@gmail.com>
2014-07-25 02:19:56 -0700

    Add notification sample in wearable app

--- wear/src/main/java/com/polysfactory/androidwearsamplejp/NotificationEmbeddedActivity.java
+++ wear/src/main/java/com/polysfactory/androidwearsamplejp/NotificationEmbeddedActivity.java
@@ -14,9 +14,13 @@ public class NotificationEmbeddedActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
 
+        TextView textView = (TextView) findViewById(R.id.text);
         Intent intent = getIntent();
         int count = intent.getIntExtra(EXTRA_KEY_COUNT, -1);
-        TextView textView = (TextView) findViewById(R.id.text);
-        textView.setText(String.format("count is %d", count));
+        if (count >= 0) {
+            textView.setText(String.format("count is %d", count));
+        } else {
+            textView.setText(String.format("Hello world!"));
+        }
     }
 }
